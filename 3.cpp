0c80c4f640963b3aecade7790005f81b4fab99fc
YJS <YJS@users.noreply.github.com>
2026-09-16 00:14:27 +0900

    초기 커밋.

---
+++ 3.cpp
@@ -0,0 +1,171 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <tuple>
+
+struct Data {
+	int x, y, z;
+};
+
+void print(std::vector<std::pair<bool, Data>> list) {
+	for (int i = list.size()-1; i >=0; i--) {
+		std::cout << "[" << i << "]\t";
+		if (list[i].first == false) {
+			std::cout << "[빈칸]" << std::endl;
+		}
+		else {
+			std::cout << "[" << list[i].second.x << ", "<< list[i].second.y << ", " << list[i].second.z << "]" << std::endl;
+		}
+	}
+}
+
+int main() {
+	std::vector<std::pair<bool, Data>> list(10, { false,{0,0,0}});
+
+	while (1) {
+		print(list);
+		char order = '\0';
+
+		std::cout << "Give Order: ";
+
+		std::cin >> order; 
+
+		int a, b, c;
+		system("cls");
+
+		if (order == '+') {
+			std::cin >> a >> b >> c;
+			//값이 들어있지 않다면
+			if (list[9].first == false) {
+				list[9] = { true, {a, b, c} };
+			}
+			//값이 들어있다면
+			else {
+				for (int i = 1; i < 10; i++) {
+					list[i - 1] = list[i];
+				}
+				list[9] = { true, {a, b, c} };
+			}
+
+		}
+		else if (order == '-') {
+			for (int i = 9; i >= 1; i--) {
+				list[i] = list[i - 1];
+			}
+			list[0].first = false;
+		}
+		else if (order == 'e') {
+			std::cin >> a >> b >> c;
+			if (list[0].first == false) {
+				list[0] = { true, {a,b,c} };
+			}
+			else {
+				for (int i = 9; i >= 1; i--) {
+					list[i] = list[i - 1];
+				}
+				list[0] = { true, {a,b,c} };
+			}
+		}
+		//맨 아래 삭제
+		else if (order == 'd') {
+			for (int i = 1; i <=9; i++) {
+				list[i - 1] = list[i];
+			}
+			list[9].first = false;
+		}
+		//개수 출력
+		else if (order == 'a') {
+			int count = 0;
+			for (auto p : list) {
+				if (p.first == true)
+					count++;
+			}
+			std::cout << "개수: " << count << std::endl;
+		}
+		//리스트 내리기
+		else if (order == 'b') {
+			bool dump_b = list[0].first;
+			Data dump_d = list[0].second;
+			for (int i = 1; i <=9; i++) {
+				list[i - 1] = list[i];
+			}
+			list[9] = { dump_b, {dump_d} };
+		}
+		//초기화
+		else if (order == 'c') {
+			list.assign(10, { false, {0,0,0}});
+		}
+		//각점에서원점과의거리를계산후, 그값을정렬하여오름차순으로정렬하여출력한다. 인덱스0번부터빈칸없이저장하여출력한다.  리스트각칸의옆에는해당칸의점과원점과의거리를출력한다.다시누르면원래대로출력한다
+		else if (order == 'f') {
+			std::vector<std::pair<int, Data>> only_f;
+			for (auto p : list) {
+				if (p.first == true) {
+					only_f.push_back(
+						{ std::pow(p.second.x, 2) + std::pow(p.second.y, 2) + std::pow(p.second.z, 2), 
+						p.second });
+					
+				}
+			}
+			std::sort(only_f.begin(), only_f.end(),
+				[](const std::pair<int, Data>& a, const std::pair<int, Data>& b) {
+					return a.first < b.first;
+				});
+
+			while (1) {
+				for (int i = 0; i < only_f.size(); i++) {
+					std::cout << only_f[i].first << " [" << only_f[i].second.x << ", " << only_f[i].second.y << ", " << only_f[i].second.z << "]" << std::endl;
+				}
+				char back = '\0';
+				std::cout << "f를 다시 입력해주세요: ";
+				std::cin >> back;
+				system("cls");
+				if (back == 'f' or back == 'F') {
+					break;
+				}
+			}
+		}
+		//리스트에저장된점들에서두점간의모든조합에대한거리를계산하고가장먼두점, 가장가까운두점을출력한다.  이때, 두점의좌표 값과그점사이의거리, 가장먼두점, 가장가까운두점을출력하고그점간의거리도출력
+		else if (order == 'g') {
+			std::vector<std::tuple<double, Data, Data>> only_g;
+			std::vector<Data> crossway;
+			for (auto p : list) {
+				if (p.first == true)
+					crossway.push_back({p.second.x, p.second.y, p.second.z});
+			}
+			for (int i = 0; i < crossway.size(); i++) {
+				for (int j = i+1; j < crossway.size(); j++) {
+					double calculation = std::sqrt(
+						std::pow(crossway[i].x - crossway[j].x, 2) + std::pow(crossway[i].y - crossway[j].y, 2) + std::pow(crossway[i].z - crossway[j].z, 2));
+					only_g.push_back({ calculation, crossway[i], crossway[j] });
+				}
+			}
+			std::sort(only_g.begin(), only_g.end(),
+				[](const auto& a, const auto& b) {
+					return std::get<0>(a) < std::get<0>(b);
+				});
+			for (auto p : only_g) {
+				std::cout << "[" << std::get<0>(p) <<
+					"]\t[" << std::get<1>(p).x << ", " << std::get<1>(p).y << ", " << std::get<1>(p).z <<
+					"]\t[" << std::get<2>(p).x << ", " << std::get<2>(p).y << ", " << std::get<2>(p).z << "]" << std::endl;
+			}
+			std::cout << "가장 가까운 두 점:\t" << "[" << std::get<0>(only_g.front()) <<
+				"]\t[" << std::get<1>(only_g.front()).x << ", " << std::get<1>(only_g.front()).y << ", " << std::get<1>(only_g.front()).z <<
+				"]\t[" << std::get<2>(only_g.front()).x << ", " << std::get<2>(only_g.front()).y << ", " << std::get<2>(only_g.front()).z << "]" << std::endl;
+
+			std::cout << "가장 먼 두 점:\t" << "[" << std::get<0>(only_g.back()) <<
+				"]\t[" << std::get<1>(only_g.back()).x << ", " << std::get<1>(only_g.back()).y << ", " << std::get<1>(only_g.back()).z <<
+				"]\t[" << std::get<2>(only_g.back()).x << ", " << std::get<2>(only_g.back()).y << ", " << std::get<2>(only_g.back()).z << "]" << std::endl;
+		}
+		//종료
+		else if (order == 'q') {
+			break;
+		}
+		else {
+			std::cout << "cannot find order" << std::endl;
+		}
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+	return 0;
+}
---
+++ 6.cpp
@@ -0,0 +1,385 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+struct Vertex {
+	double x, y, z;
+};
+
+struct Texture {
+	double s, t;
+};
+
+struct Face {
+	int vertex[3];
+	int texture[3];
+
+	bool hasTexture;
+	bool valid;
+
+	std::string error;
+};
+
+int main() {
+	std::vector<Vertex> vertex;
+	std::vector<Texture> texture;
+	std::vector<Face> face;
+
+	std::ifstream file("data_runtime.txt");
+
+	if (!file.is_open()) {
+		std::cout << "cannot open data6.txt" << std::endl;
+		return 0;
+	}
+
+	std::string line;
+
+	while (std::getline(file, line)) {
+
+		//주석 제거
+		size_t comment = line.find('#');
+
+		if (comment != std::string::npos) {
+			line = line.substr(0, comment);
+		}
+
+		std::stringstream ss(line);
+
+		std::string type;
+
+		if (!(ss >> type)) {
+			continue;
+		}
+
+		//정점 좌표값
+		if (type == "v") {
+			Vertex temp;
+
+			std::string extra;
+
+			if (!(ss >> temp.x >> temp.y >> temp.z)) {
+				std::cout << "Error: invalid character value" << std::endl;
+				continue;
+			}
+
+			if (ss >> extra) {
+				std::cout << "Error: invalid character value" << std::endl;
+				continue;
+			}
+
+			//정점 좌표 범위 확인
+			if (temp.x < -1.0 or temp.x > 1.0
+				or temp.y < -1.0 or temp.y > 1.0
+				or temp.z < -1.0 or temp.z > 1.0) {
+
+				std::cout << "Error: vertex value out of range" << std::endl;
+				continue;
+			}
+
+			vertex.push_back(temp);
+		}
+
+		//텍스처 좌표값
+		else if (type == "vt") {
+			Texture temp;
+
+			std::string extra;
+
+			if (!(ss >> temp.s >> temp.t)) {
+				std::cout << "Error: invalid character value" << std::endl;
+				continue;
+			}
+
+			if (ss >> extra) {
+				std::cout << "Error: invalid character value" << std::endl;
+				continue;
+			}
+
+			//텍스처 좌표 범위 확인
+			if (temp.s < 0.0 or temp.s > 1.0
+				or temp.t < 0.0 or temp.t > 1.0) {
+
+				std::cout << "Error: texture value out of range" << std::endl;
+				continue;
+			}
+
+			texture.push_back(temp);
+		}
+
+		//삼각형 면
+		else if (type == "f") {
+			Face temp = { {0,0,0}, {0,0,0}, false, true, "" };
+
+			std::vector<std::string> data;
+			std::string value;
+
+			while (ss >> value) {
+				data.push_back(value);
+			}
+
+			//삼각형 꼭짓점 3개 확인
+			if (data.size() != 3) {
+				temp.valid = false;
+				temp.error = "triangle needs 3 vertices";
+
+				face.push_back(temp);
+
+				continue;
+			}
+
+			bool textureUse = false;
+			bool textureNotUse = false;
+
+			for (int i = 0; i < 3; i++) {
+
+				size_t slash = data[i].find('/');
+
+				//vertex index만 있는 경우
+				if (slash == std::string::npos) {
+
+					textureNotUse = true;
+
+					std::stringstream valueStream(data[i]);
+
+					int vertexIndex;
+					char extra;
+
+					if (!(valueStream >> vertexIndex)) {
+						temp.valid = false;
+						temp.error = "invalid character value";
+						break;
+					}
+
+					if (valueStream >> extra) {
+						temp.valid = false;
+						temp.error = "invalid character value";
+						break;
+					}
+
+					temp.vertex[i] = vertexIndex;
+				}
+
+				//vertex/texture index가 있는 경우
+				else {
+
+					textureUse = true;
+
+					// /가 두 개 이상 있는 경우
+					if (data[i].find('/', slash + 1) != std::string::npos) {
+						temp.valid = false;
+						temp.error = "invalid character value";
+						break;
+					}
+
+					std::string vertexData = data[i].substr(0, slash);
+					std::string textureData = data[i].substr(slash + 1);
+
+					if (vertexData.empty() or textureData.empty()) {
+						temp.valid = false;
+						temp.error = "invalid character value";
+						break;
+					}
+
+					std::stringstream vertexStream(vertexData);
+					std::stringstream textureStream(textureData);
+
+					int vertexIndex;
+					int textureIndex;
+					char extra;
+
+					if (!(vertexStream >> vertexIndex)) {
+						temp.valid = false;
+						temp.error = "invalid character value";
+						break;
+					}
+
+					if (vertexStream >> extra) {
+						temp.valid = false;
+						temp.error = "invalid character value";
+						break;
+					}
+
+					if (!(textureStream >> textureIndex)) {
+						temp.valid = false;
+						temp.error = "invalid character value";
+						break;
+					}
+
+					if (textureStream >> extra) {
+						temp.valid = false;
+						temp.error = "invalid character value";
+						break;
+					}
+
+					temp.vertex[i] = vertexIndex;
+					temp.texture[i] = textureIndex;
+				}
+			}
+
+			//texture 사용 형식이 섞인 경우
+			if (textureUse and textureNotUse) {
+				temp.valid = false;
+				temp.error = "invalid character value";
+			}
+
+			if (textureUse and !textureNotUse) {
+				temp.hasTexture = true;
+			}
+
+			face.push_back(temp);
+		}
+
+		//허용되지 않는 문자값
+		else {
+			std::cout << "Error: invalid character value" << std::endl;
+		}
+	}
+
+	file.close();
+
+	//정점 좌표값 중복 확인
+	bool duplicate = false;
+
+	for (int i = 0; i < (int)vertex.size(); i++) {
+
+		for (int j = i + 1; j < (int)vertex.size(); j++) {
+
+			if (vertex[i].x == vertex[j].x
+				and vertex[i].y == vertex[j].y
+				and vertex[i].z == vertex[j].z) {
+
+				duplicate = true;
+			}
+		}
+	}
+
+	//face 예외 처리
+	for (int i = 0; i < (int)face.size(); i++) {
+
+		if (!face[i].valid) {
+			continue;
+		}
+
+		//같은 vertex index 확인
+		if (face[i].vertex[0] == face[i].vertex[1]
+			or face[i].vertex[0] == face[i].vertex[2]
+			or face[i].vertex[1] == face[i].vertex[2]) {
+
+			face[i].valid = false;
+			face[i].error = "duplicate vertex index";
+
+			continue;
+		}
+
+		//vertex index 범위 확인
+		for (int j = 0; j < 3; j++) {
+
+			if (face[i].vertex[j] < 1
+				or face[i].vertex[j] > (int)vertex.size()) {
+
+				face[i].valid = false;
+				face[i].error = "vertex index out of range";
+
+				break;
+			}
+		}
+
+		if (!face[i].valid) {
+			continue;
+		}
+
+		//texture index 범위 확인
+		if (face[i].hasTexture) {
+
+			for (int j = 0; j < 3; j++) {
+
+				if (face[i].texture[j] < 1
+					or face[i].texture[j] > (int)texture.size()) {
+
+					face[i].valid = false;
+					face[i].error = "texture index out of range";
+
+					break;
+				}
+			}
+		}
+
+		if (!face[i].valid) {
+			continue;
+		}
+
+		//삼각형 좌표값 중복 확인
+		Vertex v1 = vertex[face[i].vertex[0] - 1];
+		Vertex v2 = vertex[face[i].vertex[1] - 1];
+		Vertex v3 = vertex[face[i].vertex[2] - 1];
+
+		if ((v1.x == v2.x and v1.y == v2.y and v1.z == v2.z)
+			or (v1.x == v3.x and v1.y == v3.y and v1.z == v3.z)
+			or (v2.x == v3.x and v2.y == v3.y and v2.z == v3.z)) {
+
+			face[i].valid = false;
+			face[i].error = "duplicate vertex value";
+		}
+	}
+
+	//face 출력
+	for (int i = 0; i < (int)face.size(); i++) {
+
+		if (!face[i].valid) {
+
+			std::cout << "Face " << i + 1
+				<< " Error: " << face[i].error
+				<< std::endl;
+
+			continue;
+		}
+
+		std::cout << "Face " << i + 1 << " ("
+			<< face[i].vertex[0] << ", "
+			<< face[i].vertex[1] << ", "
+			<< face[i].vertex[2] << "): ";
+
+		std::cout << "vertex ";
+
+		for (int j = 0; j < 3; j++) {
+
+			int index = face[i].vertex[j] - 1;
+
+			std::cout << "("
+				<< vertex[index].x << ", "
+				<< vertex[index].y << ", "
+				<< vertex[index].z << ") ";
+		}
+
+
+		//texture가 있는 경우
+		if (face[i].hasTexture) {
+
+			std::cout << "texture ";
+
+			for (int j = 0; j < 3; j++) {
+
+				int index = face[i].texture[j] - 1;
+
+				std::cout << "("
+					<< texture[index].s << ", "
+					<< texture[index].t << ") ";
+			}
+
+		}
+
+		std::cout << std::endl;
+	}
+
+	//중복 정점 출력
+	if (!duplicate) {
+		std::cout << "No duplicate vertex value" << std::endl;
+	}
+	else {
+		std::cout << "Duplicate vertex value" << std::endl;
+	}
+
+	return 0;
+}
